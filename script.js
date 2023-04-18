88eec1a233af106a8239fc9504430ad7fa002f0f
dhanraj-saur <121377364+dhanraj-saur@users.noreply.github.com>
2023-04-18 19:15:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Array of API URLs to fetch data from
 const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/1",
   "https://jsonplaceholder.typicode.com/todos/2",
@@ -13,3 +12,34 @@ const apiUrls = [
 ];
 
 // You can write your code here
+const fetchData = async (apiUrls) => {
+  const response = await fetch(apiUrls);
+  return response.json();
+};
+
+// Measure the time taken to fetch data using Promise.all
+const measurePromiseAll = async () => {
+  const startTime = performance.now();
+  const promises = apiUrls.map(fetchData);
+  await Promise.all(promises);
+  const endTime = performance.now();
+  return endTime - startTime;
+};
+
+// Measure the time taken to fetch data using Promise.any
+const measurePromiseAny = async () => {
+  const startTime = performance.now();
+  const promises = apiUrls.map(fetchData);
+  await Promise.any(promises);
+  const endTime = performance.now();
+  return endTime - startTime;
+};
+
+// Call the functions and display the results
+measurePromiseAll().then((time) => {
+  document.getElementById('output-all').textContent = `${time} milliseconds`;
+});
+
+measurePromiseAny().then((time) => {
+  document.getElementById('output-any').textContent = `${time} milliseconds`;
+});
